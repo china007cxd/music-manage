36a05852c708fcca04e40f55a26864339e518ad2
chenxudong <xudong.chen@wisiglobal.com>
2022-10-26 15:36:11 +0800

    初始化music-manage

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      Object.assign(definitions[0]['process.env'], {
+        NODE_HOST: '"http://47.106.135.3:10000/music"',
+      });
+      return definitions;
+    });
+  }
+})
